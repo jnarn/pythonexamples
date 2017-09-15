cc5112682a82337826c1518a91012a805466a5c6
jnarn <31920752+jnarn@users.noreply.github.com>
2017-09-15 07:53:15 -0400

    Update myRandNumGen.py

--- myRandNumGen.py
+++ myRandNumGen.py
@@ -3,3 +3,8 @@
 import random
 print(random.__file__) #checking the referred module file here
 print('Random result: ' + str(random.randint(1,6))) #printing a random number between 1 to 6
+
+colorlist = ['Red','Orange','Yellow','Green','Blue','Indigo','Violet']
+
+print('Sample of 2 colours using sample method: ',random.sample(colorlist,2)) 
+
